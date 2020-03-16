72aac1d22aa921a7bcc84106a3a827070d6e9478
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-03-16 14:59:00 -0500

    Corrected spelling errors in About me.

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -10,14 +10,15 @@ function AboutPage(props) {
       <Content>
         I am a software developer specializing in the MERN stack. Please click
         on the portfolio tab to see some of my work. <br /> <br />
-        Prior to working on software, started my IT career as a level 1 helpdesk
-        technician and worked my way to serving as a Junior System
-        Administrator. Being self-motivated and flexible were the keys to
-        success while working my way up. If you would like someone with those
-        traits on your team, or to learn more about me, please feel free to
-        contact me. <br /> <br />
-        In my spare time, I am an avid dog walker. See the "Paw"some pics tab
-        for my walking buddies.
+        Prior to working on software, I started my technology career as a level
+        1 helpdesk technician and worked my way to serving as a Junior System
+        Administrator. Being self-motivated and flexible were the greatest
+        factors to my success. If you would like someone with those traits on
+        your team, or to learn more about me, please feel free to contact me.{' '}
+        <br /> <br />
+        In my spare time, I am an avid dog walker and video gamer. See the
+        "Paw"some pics tab for my walking buddies. If you look at my portfolio,
+        you will see some of what I enjoy about gaming.
       </Content>
     </div>
   );
